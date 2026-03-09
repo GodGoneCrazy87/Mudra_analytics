7bfb25f4171ae1021c93a5ff9726a718145b8ae2
Vishnurat_Kadagadakai <127509213+GodGoneCrazy87@users.noreply.github.com>
2026-03-09 23:57:38 +0530

    Update ClientLayout.js

--- src/app/ClientLayout.js
+++ src/app/ClientLayout.js
@@ -18,3 +18,23 @@ export default function ClientLayout({ children }) {
     </>
   );
 }
+"use client";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation"; // ✅ Detect page changes
+import Navbar from "./components/Navbar";
+
+export default function ClientLayout({ children }) {
+  const pathname = usePathname(); // ✅ Detect page changes
+
+
+  return (
+    <>
+        
+        <>
+          <Navbar />
+          <main>{children}</main>
+        </>
+     
+    </>
+  );
+}
