8af1340fc48509a0632afe95bcf7ea8d4fe66578
Vishnurat_Kadagadakai <127509213+GodGoneCrazy87@users.noreply.github.com>
2026-03-11 22:58:34 +0530

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,3 +5,10 @@ import { About } from '../components';
 const AboutPage = () => <About />;
 
 export default AboutPage;
+// app/about/page.jsx
+
+import { About } from '../components';
+
+const AboutPage = () => <About />;
+
+export default AboutPage;
