cd16c77a0ed5befb717f1eca565e1ee863df13cb
Vishnurat_Kadagadakai <Vishnurat87@gmail.com>
2026-03-16 21:50:34 +0530

    Update page.jsx

--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -12,3 +12,10 @@ import { Results } from '../components';
 const ResultsPage = () => <Results />;
 
 export default ResultsPage;
+// app/results/page.jsx
+
+import { Results } from '../components';
+
+const ResultsPage = () => <Results />;
+
+export default ResultsPage;
