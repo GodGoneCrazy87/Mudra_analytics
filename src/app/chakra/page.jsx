c32528f69cb5d5c28835097af8236ad37b1ebf9d
Vishnurat_Kadagadakai <Vishnurat87@gmail.com>
2026-03-20 23:57:53 +0530

    Update page.jsx

--- src/app/chakra/page.jsx
+++ src/app/chakra/page.jsx
@@ -12,3 +12,10 @@ import { Chakra } from '../components';
 const ChakraPage = () => <Chakra />;
 
 export default ChakraPage;
+// app/chalra/page.jsx
+
+import { Chakra } from '../components';
+
+const ChakraPage = () => <Chakra />;
+
+export default ChakraPage;
